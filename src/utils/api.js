28b08ab2779eacda337f461a136007dc1f130a77
keroles adel <kerolesadel297@gmail.com>
2026-03-07 18:59:58 +0200

    update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -10,7 +10,7 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlYjQxZjE1OS1hNDEzLTQ4Y2MtMGFiMy0wOGRlMWE1ZTMzYmQiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJEb2N0b3IiLCJlbWFpbCI6InVzZXIwQGV4YW1wbGUuY29tIiwiUGhvbmVOdW1iZXIiOiIwMTExOTc0ODk4IiwiZXhwIjoxNzczNTAxODgzLCJpc3MiOiJEb2N0b3JNYXRlQVBJIiwiYXVkIjoiRG9jdG9yTWF0ZUNsaWVudCJ9.iiBMproLfV5HltqObs5VVDcOChkmrm54-5RDKqb3Y9E`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
